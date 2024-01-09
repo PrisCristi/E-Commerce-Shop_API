2e8ec42f08fcaab70e47c569b129d4fc676c2d0d
PrisCristi <santos.priscila85@gmail.com>
2024-01-09 15:29:17 +0100

    Inicial commit

---
+++ src/test/java/com/ecomerceApi/Priscila/PriscilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerceApi.Priscila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriscilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
