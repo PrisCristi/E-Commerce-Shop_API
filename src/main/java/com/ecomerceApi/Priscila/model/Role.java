e71e162789d6aa1b54335fde7b6c62f55286b799
PrisCristi <santos.priscila85@gmail.com>
2024-01-15 15:13:38 +0100

    FPM-00 Added permission to enum Role

--- src/main/java/com/ecomerceApi/Priscila/model/Role.java
+++ src/main/java/com/ecomerceApi/Priscila/model/Role.java
@@ -1,14 +1,39 @@
 package com.ecomerceApi.Priscila.model;
 
-import jakarta.persistence.*;
-
-import lombok.Getter;
-import lombok.Setter;
+import java.util.Collections;
+import java.util.Set;
 
 public enum Role {
-    CUSTOMER,
-    USER,
-    ADMIN
+    USER(Collections.emptySet()),
+    CUSTOMER(Set.of(
 
-    ;
+            Permission.CUSTOMER_READ,
+            Permission.CUSTOMER_UPDATE,
+            Permission.CUSTOMER_DELETE,
+            Permission.CUSTOMER_CREATE
+    )),
+
+    ADMIN(Set.of(
+            Permission.ADMIN_CREATE,
+            Permission.ADMIN_UPDATE,
+            Permission.ADMIN_READ,
+            Permission.ADMIN_DELETE,
+
+            Permission.CUSTOMER_CREATE,
+            Permission.CUSTOMER_UPDATE,
+            Permission.CUSTOMER_READ,
+            Permission.CUSTOMER_DELETE
+
+    ));
+
+
+    private final Set<Permission> permissions; // to no have duplications set is better.
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
 }
