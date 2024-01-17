1c4d9712e38d37a5188479d2f90ddbcafed99914
PrisCristi <santos.priscila85@gmail.com>
2024-01-17 10:25:04 +0100

    FPM-00 TokenRepository created

--- src/main/java/com/ecomerceApi/Priscila/model/Token.java
+++ src/main/java/com/ecomerceApi/Priscila/model/Token.java
@@ -1,5 +1,33 @@
 package com.ecomerceApi.Priscila.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
 
 }
---
+++ src/main/java/com/ecomerceApi/Priscila/model/TokenType.java
@@ -0,0 +1,5 @@
+package com.ecomerceApi.Priscila.model;
+
+public enum TokenType {
+    BEARER
+}
