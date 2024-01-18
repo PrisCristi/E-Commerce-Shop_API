03c68c82ca75be91bbf23973814d2cde46564959
Faruk Arnaut <arnaut551@gmail.com>
2024-01-18 12:09:46 +0100

    Refactored configuration for security

--- src/main/java/com/ecomerceApi/Priscila/config/SecurityConfiguration.java
+++ src/main/java/com/ecomerceApi/Priscila/config/SecurityConfiguration.java
@@ -1,15 +1,20 @@
 package com.ecomerceApi.Priscila.config;
 
-import com.ecomerceApi.Priscila.model.Permission;
-import com.ecomerceApi.Priscila.service.UserService;
-import com.mysql.cj.protocol.AuthenticationProvider;
+import com.ecomerceApi.Priscila.EUserDetailsService;
+import com.ecomerceApi.Priscila.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,15 +23,39 @@ import static com.ecomerceApi.Priscila.model.Role.ADMIN;
 import static com.ecomerceApi.Priscila.model.Role.CUSTOMER;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final UserService userService;
-    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    UserRepository repository;
+
+    @Autowired
+    EUserDetailsService userDetailsService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         HttpSecurity httpSecurity = http
                 .csrf()
                 .disable()
@@ -34,29 +63,24 @@ public class SecurityConfiguration {
                 .requestMatchers("/ap1/v1/auth **")
                 .permitAll()
                 .requestMatchers("/ap1/v1/admin **").hasAnyRole(ADMIN.name())
-
                 .requestMatchers(HttpMethod.GET, "/ap1/v1/admin **").hasAnyAuthority(ADMIN_READ.name(), CUSTOMER_READ.name())
-                .requestMatchers(HttpMethod.POST, "/ap1/v1/admin **").hasAnyAuthority(ADMIN_CREATE.name(),CUSTOMER_CREATE.name())
+                .requestMatchers(HttpMethod.POST, "/ap1/v1/admin **").hasAnyAuthority(ADMIN_CREATE.name(), CUSTOMER_CREATE.name())
                 .requestMatchers(HttpMethod.PUT, "/ap1/v1/admin **").hasAnyAuthority(ADMIN_UPDATE.name(), CUSTOMER_UPDATE.name())
                 .requestMatchers(HttpMethod.DELETE, "/ap1/v1/admin **").hasAnyAuthority(ADMIN_DELETE.name(), CUSTOMER_DELETE.name())
-
                 .requestMatchers("/ap1/v1/customer **").hasRole(CUSTOMER.name())
-
                 .requestMatchers(HttpMethod.GET, "/ap1/v1/customer **").hasAuthority(CUSTOMER_READ.name())
                 .requestMatchers(HttpMethod.POST, "/ap1/v1/customer **").hasAuthority(CUSTOMER_CREATE.name())
                 .requestMatchers(HttpMethod.PUT, "/ap1/v1/customer **").hasAuthority(CUSTOMER_UPDATE.name())
                 .requestMatchers(HttpMethod.DELETE, "/ap1/v1/customer **").hasAuthority(CUSTOMER_DELETE.name())
-
-
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authenticationProvider((org.springframework.security.authentication.AuthenticationProvider) authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
 }
