25dfb7f79f5743e4af0e13fa4b1348df2482801a
PrisCristi <santos.priscila85@gmail.com>
2024-01-23 15:06:31 +0100

    FPM-Add implemantation to Product Entity class

--- src/main/java/com/ecomerceApi/Priscila/model/Product.java
+++ src/main/java/com/ecomerceApi/Priscila/model/Product.java
@@ -4,8 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-    @Data
+@Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Entity
@@ -18,19 +19,26 @@ import lombok.NoArgsConstructor;
         private Long productId;
 
         @Column(name = "product_name")
+        @NonNull
         private String productName;
 
         @Column
         private String description;
 
         @Column
+        @NonNull
         private Double price;
 
+
+
+       /*
         @Column(name = "stock_quantity")
         private int stockQuantity;
 
         @Column(name = "is_stored")
         private boolean stored;
+
+        */
 
 
 
