92003f1829600415b5f5af922a855f8e889c946d
PrisCristi <santos.priscila85@gmail.com>
2024-01-24 10:34:36 +0100

    FPM-13 Change annotation @Controller to @RestController

--- src/main/java/com/ecomerceApi/Priscila/controller/UserController.java
+++ src/main/java/com/ecomerceApi/Priscila/controller/UserController.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping ("/user")
 public class UserController {
     UserService userService;
