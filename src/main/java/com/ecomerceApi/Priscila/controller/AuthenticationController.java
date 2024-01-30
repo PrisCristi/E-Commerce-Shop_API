d9b792a43f49c7ef28bd9a535b2fec70c6a96aeb
PrisCristi <santos.priscila85@gmail.com>
2024-01-30 11:16:59 +0100

    FPM-00- requestMapping Removed

--- src/main/java/com/ecomerceApi/Priscila/controller/AuthenticationController.java
+++ src/main/java/com/ecomerceApi/Priscila/controller/AuthenticationController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
 @AllArgsConstructor
 public class AuthenticationController {  // authenticate Login access.
 
