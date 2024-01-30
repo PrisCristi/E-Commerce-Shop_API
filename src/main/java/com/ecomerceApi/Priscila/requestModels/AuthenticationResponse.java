2f28dcf3c39e970b72ea6ac683658812968232d8
PrisCristi <santos.priscila85@gmail.com>
2024-01-30 11:22:03 +0100

    FPM-00 - Annotation value excluded

--- src/main/java/com/ecomerceApi/Priscila/requestModels/AuthenticationResponse.java
+++ src/main/java/com/ecomerceApi/Priscila/requestModels/AuthenticationResponse.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Value;
 import org.springdoc.core.configuration.oauth2.SpringDocOAuth2Token;
 
-@Value
 @Builder
 public class AuthenticationResponse implements SpringDocOAuth2Token {
 
