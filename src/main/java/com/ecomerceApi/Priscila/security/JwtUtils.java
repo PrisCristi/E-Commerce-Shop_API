2ade2a4782f378b5c7361e67369959384f84097d
PrisCristi <santos.priscila85@gmail.com>
2024-02-07 16:56:55 +0100

    JwtUtilits implemented

--- src/main/java/com/ecomerceApi/Priscila/security/JwtUtils.java
+++ src/main/java/com/ecomerceApi/Priscila/security/JwtUtils.java
@@ -1,4 +1,67 @@
 package com.ecomerceApi.Priscila.security;
 
+import com.ecomerceApi.Priscila.service.UserDetalsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import static jdk.internal.org.jline.keymap.KeyMap.key;
+import static org.aspectj.bridge.Version.getTime;
+
+@Component
 public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${priscila.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${priscila.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserDetalsImpl userPrincipal = (UserDetalsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.ES512)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor((Decoders.BASE64.decode(jwtSecret)));
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: ", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Token is expired ", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token not supported ", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("Token claims empty string ", e.getMessage());
+        }
+        return false;
+    }
 }
+
