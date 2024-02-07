282603b8b30ef35132c4a317f27b9a2884f6afcf
PrisCristi <santos.priscila85@gmail.com>
2024-02-07 16:11:27 +0100

    UserDetailsSrviceImpl implemented

--- src/main/java/com/ecomerceApi/Priscila/service/UserDetailsServiceImpl.java
+++ src/main/java/com/ecomerceApi/Priscila/service/UserDetailsServiceImpl.java
@@ -1,5 +1,24 @@
 package com.ecomerceApi.Priscila.service;
 
-public class UserDetailsServiceImpl {
+import com.ecomerceApi.Priscila.model.User;
+import com.ecomerceApi.Priscila.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    UserRepository userRepository;
+    User user;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserDetalsImpl.build(user);
+    }
 
 }
