d7cd403e7eeae9367cb5d6b892b7b821ef934e3a
PrisCristi <santos.priscila85@gmail.com>
2024-02-17 18:31:28 +0100

    Cart and CartItem repository implemented

--- src/main/java/com/ecomerceApi/Priscila/repository/CartRepository.java
+++ src/main/java/com/ecomerceApi/Priscila/repository/CartRepository.java
@@ -2,18 +2,14 @@
 package com.ecomerceApi.Priscila.repository;
 
 import com.ecomerceApi.Priscila.model.Cart;
-import com.ecomerceApi.Priscila.model.CartItem;
-import com.ecomerceApi.Priscila.model.Product;
-import com.ecomerceApi.Priscila.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+
+    // TODO: 17.02.24 check the medothes bellow are necessary
 //    Optional<CartItem> getCartByUserAndProduct(User user, Product product);
 //    List<CartItem> getCartsByUser(User user);
 }
